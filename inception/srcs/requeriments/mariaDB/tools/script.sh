96e28c704239e31e469083c74d76e425a6ded464
jlimonesHer <josec.limones@gmail.com>
2023-11-09 10:12:23 +0100

    funciona mariadb, falta conf de wordpress

--- inception/srcs/requeriments/mariaDB/tools/script.sh
+++ inception/srcs/requeriments/mariaDB/tools/script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# mysql_install_db
 # Iniciar el servicio MySQL
 service mysql start
 # Esperar a que el servicio MySQL se inicie completamente
@@ -7,24 +6,23 @@ service mysql start
 #     echo "esperando"
 #     sleep 1
 # done
-# mysql -e "FLUSH PRIVILEGES;"
-echo "FLUSH 1"
-sleep 5
-mysql -e "CREATE DATABASE IF NOT EXISTS '$WORDPRESS_DB_NAME';"
-
-echo "base de datos creada"
-sleep 5
-mysql -e "CREATE USER IF NOT EXISTS '$WORDPRESS_DB_USER'@'localhost' IDENTIFIED BY '${WORDPRESS_DB_PASSWORD}';"
-echo "usuario creado"
-sleep 5
-mysql -e "GRANT ALL PRIVILEGES ON '${WORDPRESS_DB_NAME}'.* TO '${WORDPRESS_DB_USER}' IDENTIFIED BY '${WORDPRESS_DB_PASSWORD}'"
-echo "Privilegios dados"
-sleep 5
-echo $WORDPRESS_DB_PASSWORD
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${WORDPRESS_DB_PASSWORD}';"
-sleep 5
 mysql -e "FLUSH PRIVILEGES;"
-echo "Privilegios actualizados"
-sleep 5
+echo "FLUSH 1"
+# sleep 5
+mysql -e "CREATE DATABASE IF NOT EXISTS $WORDPRESS_DB_NAME;"
+echo "base de datos creada"
+# sleep 5
+mysql -e "CREATE USER IF NOT EXISTS '$WORDPRESS_DB_USER@wordpress.srcs_incetion' IDENTIFIED BY '$WORDPRESS_DB_PASSWORD';"
+echo "Ususario creado"
+# sleep 5
+mysql -e "GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO $WORDPRESS_DB_USER IDENTIFIED BY '$WORDPRESS_DB_PASSWORD';"
+echo "Privilegios dados GRANT"
+# sleep 5
+mysql -e "ALTER USER '$WORDPRESS_DB_USER'@'%' IDENTIFIED BY '$WORDPRESS_DB_PASSWORD';"
+echo "ALTER"
+# sleep 5
+mysql -e "FLUSH PRIVILEGES;"
+echo "Privilegios actualizados FLUSH 2"
+# sleep 5
 echo "Fin del script"
 
--- inception/srcs/requeriments/wordpress/tools/auto_config.sh
+++ inception/srcs/requeriments/wordpress/tools/auto_config.sh
@@ -4,6 +4,8 @@ sleep 3
 cd /var/www/wordpress
 
 # Descargar WP-CLI
+if ! [[ -f /var/www/wordpress/wp-config.php ]]
+ then
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
@@ -11,8 +13,7 @@ mv wp-cli.phar /usr/local/bin/wp
 # Verificar si wp-config.php existe
 cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
 
-# if [ ! -f wp-config.php ]; then
-    # Crear el archivo wp-config.php si no existe
+    #Crear el archivo wp-config.php si no existe
 
 	cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
 	sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/g" /var/www/wordpress/wp-config.php
@@ -26,8 +27,8 @@ cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
 
 	wp user create $WORDPRESS_DB_USER $WORDPRESS_DB_EMAIL --user_pass=$WORDPRESS_DB_PASSWORD \
 		--role=author --allow-root --path=/var/www/wordpress
-# else
-#     echo "El archivo wp-config.php ya existe."
-# fi
+else
+    echo "El archivo wp-config.php ya existe."
+fi
 # Iniciar el servidor PHP-FPM (usando la ruta completa)
 /usr/sbin/php-fpm7.3 -y /etc/php/7.3/fpm/php-fpm.conf -F
