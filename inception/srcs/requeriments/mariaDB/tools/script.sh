f83e29bd9dadae684f07b8d6635035701541c7a9
jlimonesHer <josec.limones@gmail.com>
2023-11-09 19:16:10 +0100

    mirar tools/.env

--- inception/srcs/requeriments/mariaDB/tools/script.sh
+++ inception/srcs/requeriments/mariaDB/tools/script.sh
@@ -2,27 +2,15 @@
 # Iniciar el servicio MySQL
 service mysql start
 # Esperar a que el servicio MySQL se inicie completamente
-# while ! mysqladmin ping -hlocalhost --silent; do
-#     echo "esperando"
-#     sleep 1
-# done
+while ! mysqladmin ping -hlocalhost --silent; do
+    echo "esperando"
+    sleep 1
+done
 mysql -e "FLUSH PRIVILEGES;"
-echo "FLUSH 1"
-# sleep 5
 mysql -e "CREATE DATABASE IF NOT EXISTS $WORDPRESS_DB_NAME;"
-echo "base de datos creada"
-# sleep 5
 mysql -e "CREATE USER IF NOT EXISTS '$WORDPRESS_DB_USER@wordpress.srcs_incetion' IDENTIFIED BY '$WORDPRESS_DB_PASSWORD';"
-echo "Ususario creado"
-# sleep 5
 mysql -e "GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO $WORDPRESS_DB_USER IDENTIFIED BY '$WORDPRESS_DB_PASSWORD';"
-echo "Privilegios dados GRANT"
-# sleep 5
 mysql -e "ALTER USER '$WORDPRESS_DB_USER'@'%' IDENTIFIED BY '$WORDPRESS_DB_PASSWORD';"
-echo "ALTER"
-# sleep 5
 mysql -e "FLUSH PRIVILEGES;"
-echo "Privilegios actualizados FLUSH 2"
-# sleep 5
-echo "Fin del script"
+
 
--- inception/srcs/requeriments/wordpress/tools/auto_config.sh
+++ inception/srcs/requeriments/wordpress/tools/auto_config.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
-sleep 3
-# Cambiar al directorio de WordPress
-cd /var/www/wordpress
-
-# Descargar WP-CLI
 if ! [[ -f /var/www/wordpress/wp-config.php ]]
  then
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
-
-# Verificar si wp-config.php existe
-cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
-
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	mv wp-cli.phar /usr/local/bin/wp
+	# Verificar si wp-config.php existe
+	cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
     #Crear el archivo wp-config.php si no existe
-
 	cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
 	sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/g" /var/www/wordpress/wp-config.php
 	sed -i "s/localhost/mariadb.srcs_inception/g" /var/www/wordpress/wp-config.php
 	sed -i "s/username_here/$WORDPRESS_DB_USER/g" /var/www/wordpress/wp-config.php
 	sed -i "s/database_name_here/$WORDPRESS_DB_NAME/g" /var/www/wordpress/wp-config.php
-    wp core download --path=/var/www/wordpress --allow-root
-	wp core install --allow-root --url=jlimones.42.fr --title=$WORDPRESS_DB_USER \
+	wp core install --allow-root --url=$URL --title=$WORDPRESS_DB_USER \
 		--admin_user=$WORDPRESS_DB_USER --admin_password=$WORDPRESS_DB_PASSWORD \
 		--admin_email=$WORDPRESS_DB_EMAIL --path=/var/www/wordpress
 
